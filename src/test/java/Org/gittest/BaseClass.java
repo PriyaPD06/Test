9761ffa010581c00c4a4b9a8da6cd0f7ae2a39c5
PriyaPD06 <Ramya0404>
2021-10-21 22:04:04 +0530

    PD's updated code

--- src/test/java/Org/gittest/BaseClass.java
+++ src/test/java/Org/gittest/BaseClass.java
@@ -1,5 +1,92 @@
 package Org.gittest;
 
-public class BaseClass {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-}
+	public class BaseClass {
+	
+	public static WebDriver driver;
+		
+		public static WebDriver launchBrowser(String browserName) {
+		
+			switch(browserName) {
+			
+	        case "Chrome":
+			
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+			
+	       
+	        	default:
+	        		
+	        	System.err.println("Invalid Browser Name");
+	        	break;
+			}
+			
+			return driver;
+			
+		}
+		public static void launchUrl(String url) {
+			
+			driver.get(url);
+			
+			}
+		
+		public static void fillTextBox(WebElement e, String value) {
+			e.sendKeys(value);
+		}
+			public static void btnClick(WebElement e) {
+				e.click();
+			}
+			
+			public static void quitBrowser() {
+				driver.quit();}
+			
+			public static String getCurrentURL() {
+				String url = driver.getCurrentUrl();
+				return url;}
+			
+			public static String getAttribute(WebElement e) {
+				return e.getAttribute("Value");}
+			
+
+			public static String getText(WebElement e) {
+				return e.getText();}
+			
+			
+		public static void moveToElement(WebElement e) {
+			Actions a = new Actions(driver);
+			a.moveToElement(e).perform();
+			}
+		
+		public static void dragAndDrop(WebElement src, WebElement des) {
+			
+			Actions a = new Actions(driver);
+			a.dragAndDrop(src, des).perform();}
+		public static void selectByIndex(WebElement element, int index) {
+			
+		Select s = new Select(element);
+	    s.selectByIndex(index);
+		}
+		
+		public static WebElement findElement(String locatname, String locator) {
+			WebElement e = null;
+		if(locatname.equals("id")) {
+			e = driver.findElement(By.id(locator));
+		}
+		else if(locatname.equals("name")) {
+			e = driver.findElement(By.name(locator));
+		}
+		else if (locatname.equals("xpath")) {
+			e=driver.findElement(By.xpath(locator));
+		}
+		return e;
+			
+		}
+		}
--- src/test/java/Org/gittest/sample.java
+++ src/test/java/Org/gittest/sample.java
@@ -1,8 +1,72 @@
 package Org.gittest;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.*;
 
-public class sample {
+public class sample extends BaseClass{
 
-	public static void main(String[] args) {
-		
+	@BeforeClass
+	private void BeforeClass() {
+		launchBrowser("Chrome");
+		System.out.println("Before class");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
+	@AfterClass
+	private void afterClass() {
+		quitBrowser();
+		System.out.println("After class");
+	}
+
+	@Test
+	private void Test1() {
+		launchUrl("https://www.amazon.in/");
+		WebElement acc = driver.findElement(By.xpath("//div[@class='nav-line-1-container']"));
+		acc.click();
+		WebElement username = driver.findElement(By.id("ap_email"));
+		username.sendKeys("dharshini.pd93@gmail.com");
+		WebElement login = driver.findElement(By.id("continue"));
+		login.click();
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("Priya$06");
+		WebElement contbtn = driver.findElement(By.id("signInSubmit"));
+		contbtn.click();
+		WebElement Searchbox = driver.findElement(By.id("twotabsearchtextbox"));
+		Searchbox.sendKeys("iphone 13 pro");
+		WebElement button = driver.findElement(By.id("nav-search-submit-button"));
+		button.click();
+		System.out.println("...........Searching for IPHONE 13 PRO.............");
+	}
+	@Test
+	private void testA() {
+		System.out.println("MODEL 01 === Apple iPhone 13 Pro Max (128GB) - Sierra Blue");
+		System.out.println("Price === 1,29,900 ");
+	}
+	@Test
+	private void testB() {
+		System.out.println("MODEL 02 === Apple iPhone 13 Pro Max (256GB) - Gold");
+		System.out.println("Price === 1,39,900 ");
+	}
+	@Test
+	private void testC() {
+		System.out.println("MODEL 03 === Apple iPhone 13 (256GB) - Starlight");
+		System.out.println("PRICE === 89,900 ");
+	}
+	@Test
+	private void testD() {
+		System.out.println("MODEL 04 === Apple iPhone 13 Pro Max (128GB) - Gold");
+		System.out.println("PRICE === 1,29,900 ");
+	}
+	@Test
+	private void testE() {
+		System.out.println("MODEL 05 === Apple iPhone 13 (256GB) - Starlight");
+		System.out.println("PRICE === 89,900 ");
+	}
+	@Test
+	private void testF() {
+		System.out.println("MODEL 06 === Apple iPhone 13 (128GB) - Midnight");
+		System.out.println("PRICE === 79,900 ");
+	}
+	
 }
